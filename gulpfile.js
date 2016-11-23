95895ed2ff471ef625382915bc094712d9625739
cll5 <chuck.loong.lee@gmail.com>
2016-11-23 11:25:26 -0800

    wrote out the basic tasks for js linting and compass with the desired configurations

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 var gulp = require('gulp'),
-  sass = require('gulp-ruby-sass'),
   compass = require('gulp-compass'),
-  jshint = require('gulp-jshint'),
-  cache = require('gulp-cache'),
-  del = require('del');
+  eslint = require('gulp-eslint');
+  // cache = require('gulp-cache'),
+  // del = require('del');
   // cssnano = require('gulp-cssnano'),
   // uglify = require('gulp-uglify'),
   // imagemin = require('gulp-imagemin'),
@@ -11,19 +10,28 @@ var gulp = require('gulp'),
   // concat = require('gulp-concat'),
   // notify = require('gulp-notify');
 
+var logErrorToConsole = function(error) {
+  console.log(error);
+  this.emit('end');
+};
 
-gulp.task('default', function () {
-
-  // Compile SCSS style
-  gulp.src('sass/**/*.scss')
-    .pipe(compass({
-      config_file: './config.rb',
-      css: 'stylesheets',
-      sass: 'sass'
-    }))
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-    .pipe(gulp.dest('app/styles'));
-
-
+gulp.task('compass', function() {
+  return gulp.src('sass/**/*.scss')
+             .pipe(compass({
+                config_file: './config.rb',
+                css: 'app/styles',
+                sass: 'sass'
+             }))
+             .pipe(gulp.dest('app/styles'))
+             .on('error', logErrorToConsole);
 });
+
+gulp.task('eslint', function() {
+  return gulp.src('app/scripts/**/*.js')
+             .pipe(eslint())
+             .pipe(eslint.format())
+             .pipe(eslint.failAfterError())
+             .on('error', logErrorToConsole);
+});
+
+gulp.task('default', ['compass', 'eslint']);
